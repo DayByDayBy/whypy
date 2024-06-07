9d382140a6dfc36625ba2a3e15db990d9b031301
Diomedes Git <diomedesgit@gmail.com>
2024-06-08 00:29:21 +0100

    updating f string, adding try/catch, adding model temp  etc data to both outputs

--- prou.py
+++ prou.py
@@ -13,9 +13,10 @@ time_stamp = datetime.now().strftime("%Y%m%d_%H%M")
 initial_prompt = "what do you consider the most overrated virtue?"
 
 model_name = "llama3"
-iterations = 500
-frequency_penalty = np.float32(0.99)
-presence_penalty = np.float32(0.99)
+iterations = 3
+temp = 0.5
+frequency_penalty = np.float32(0.9)
+presence_penalty = np.float32(0.9)
 llm = Ollama(model = model_name)
 
 def respo(initial_prompt, iterations):
@@ -25,7 +26,7 @@ def respo(initial_prompt, iterations):
         response = llm.invoke(
             current_prompt, 
             max_tokens=98, 
-            temperature=0.85,
+            temperature=temp,
             frequency_penalty = float(frequency_penalty), 
             presence_penalty = float(presence_penalty))
         responses.append([i, response, '\n\n'])
@@ -38,17 +39,30 @@ indexed_responses = []
 for ip, it in enumerate(returned_responses):
     indexed_responses.append((ip, it))
 
-# print to file:
+# print to file stuff:
 
-df_output_filename = f'outputs/prou_df_{model_name}_{time_stamp}.csv'
+df_output_filename = f'outputs/prou/prou_df_{model_name}_{time_stamp}.csv'
 
 df = pd.DataFrame(indexed_responses, columns=['Index', 'Response'])
-
-with open(df_output_filename, 'w') as f:
-    f.write("# initial prompt:  " + initial_prompt + "\n\n") 
-    df.to_csv(f, index=False)
-
-output_filename = f'outputs/prou_{model_name}_{time_stamp}.txt'
-with open(output_filename, 'w') as f:
-    for index, response in indexed_responses:      
-        f.write(f'iteration {index}: \n {response}\n\n')
+try:
+    
+    with open(df_output_filename, 'w') as f:
+        f.write(f"# initial prompt: {initial_prompt}\n\n"
+                f"temp: {temp};  FP: {frequency_penalty}; PP: {presence_penalty}\n\n") 
+        df.to_csv(f, index=False)
+except Exception as e:
+    print(f"error while writing to csv file: {e}")
+    
+try: 
+    output_filename = f'outputs/prou/prou_{model_name}_{time_stamp}.txt'
+    with open(output_filename, 'w') as f:
+        f.write(f"# initial prompt: {initial_prompt}\n\n"
+                f"temp: {temp};  FP: {frequency_penalty}; PP: {presence_penalty}\n\n") 
+        for index, response in indexed_responses:      
+            f.write(f'iteration {index}: \n {response}\n\n')
+except Exception as e: 
+    print(f"error while writing to txt file: {e}")
+    
+    
+    respo(initial_prompt, iterations)
+    respo(initial_prompt, iterations)
